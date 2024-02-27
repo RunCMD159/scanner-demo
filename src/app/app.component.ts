fdcde0c273c031349080124a7cbfe87bc496e9ce
FSchmuck <flou501@gmail.com>
2024-02-27 10:32:09 +0100

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {LOAD_WASM, NgxScannerQrcodeModule} from "ngx-scanner-qrcode";
+import {AsyncPipe, JsonPipe} from "@angular/common";
 
+LOAD_WASM().subscribe();
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgxScannerQrcodeModule, AsyncPipe, JsonPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
